39a4bb46917cce8caebaaea6b690eb15af782565
강보령 <boryung@boryung-MacBookPro.local>
2025-03-02 00:10:11 +0900

    first commit

---
+++ src/main/java/com/miniproj/jeongsangLog/JeongsangLogApplication.java
@@ -0,0 +1,13 @@
+package com.miniproj.jeongsangLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeongsangLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeongsangLogApplication.class, args);
+	}
+
+}
